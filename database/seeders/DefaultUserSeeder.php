dd4d38b1cda22a76a75a7f1724e75fb7a5ad7cc1
lowkey1729 <olarewajumojeed9@gmail.com>
2023-05-15 12:28:02 +0100

    Linted existing codes

--- database/seeders/DefaultUserSeeder.php
+++ database/seeders/DefaultUserSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DefaultUserSeeder extends Seeder
@@ -19,7 +18,7 @@ class DefaultUserSeeder extends Seeder
             'email' => 'johndoe9@gmail.com',
             'phone_number' => '+2349010768387',
             'email_verified_at' => now(),
-            'password' => bcrypt('password')
+            'password' => bcrypt('password'),
         ]);
     }
 }
