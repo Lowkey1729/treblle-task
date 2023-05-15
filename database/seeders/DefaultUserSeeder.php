f2a1971a30b021d0db3d977f0f14b00e9dd9b707
lowkey1729 <olarewajumojeed9@gmail.com>
2023-05-15 12:14:56 +0100

    Created seeders, factories and migrations for the User model

---
+++ database/seeders/DefaultUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DefaultUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'johndoe9@gmail.com',
+            'phone_number' => '+2349010768387',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password')
+        ]);
+    }
+}
