92c0cc76ace1f0ee0a1c80f110dd34ad9f1f6fe9
lowkey1729 <olarewajumojeed9@gmail.com>
2023-05-15 10:44:32 +0100

    Set a consistent response for validation error via api

---
+++ app/Services/Concerns/ValidationError.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Services\Concerns;
+
+use App\Services\Helpers\ApiResponse;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\ValidationException;
+
+trait ValidationError
+{
+    /**
+     * @throws HttpResponseException
+     */
+    protected function failedValidation(Validator $validator): void
+    {
+        request()->wantsJson() ?
+            $this->apiValidationResponse($validator) :
+            parent::failedValidation($validator);
+    }
+
+    protected function apiValidationResponse(Validator $validator): void
+    {
+        throw new HttpResponseException(
+            ApiResponse::failed(
+                $validator->errors()->first(),
+                errors: $validator->errors()->toArray(),
+                httpStatusCode: 422
+            )
+        );
+    }
+}
