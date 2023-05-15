5dc05a25a86b57c4b2d5b68ffa57c469c7b47d33
lowkey1729 <olarewajumojeed9@gmail.com>
2023-05-16 00:38:46 +0100

    Register the interfaces and actions in the app provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use App\Actions\Auth\API\APIAuthSessionAction;
+use App\Actions\Auth\Web\WebAuthSessionAction;
+use App\Http\Controllers\API\Auth\AuthAPISessionController;
+use App\Http\Controllers\Web\Auth\AuthWebSessionController;
+use App\Services\Contracts\Auth\AuthInterface;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
@@ -12,7 +17,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->when(AuthWebSessionController::class)
+            ->needs(AuthInterface::class)
+            ->give(fn () => WebAuthSessionAction::class);
+
+        $this->app->when(AuthAPISessionController::class)
+            ->needs(AuthInterface::class)
+            ->give(fn () => APIAuthSessionAction::class);
     }
 
     /**
