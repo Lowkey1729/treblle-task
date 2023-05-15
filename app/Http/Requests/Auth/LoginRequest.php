2a285a14eac4df7ca4bdc5c91c68956e859c5844
lowkey1729 <olarewajumojeed9@gmail.com>
2023-05-15 23:44:52 +0100

    Imported validation trait class

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Requests\Auth;
 
+use App\Services\Concerns\ValidationError;
 use Illuminate\Foundation\Http\FormRequest;
 
 class LoginRequest extends FormRequest
 {
+    use ValidationError;
+
     /**
      * Determine if the user is authorized to make this request.
      */
