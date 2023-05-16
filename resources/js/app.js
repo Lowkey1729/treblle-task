82d2fb557dbd422154a0777ddb727f0b3848aecb
lowkey1729 <olarewajumojeed9@gmail.com>
2023-05-16 22:52:28 +0100

    Modified the setup for inertia js and vue js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,15 @@
-import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/vue3'
+import {createApp, h} from 'vue'
+import {createInertiaApp} from '@inertiajs/vue3'
 
 createInertiaApp({
-    resolve: name => require(`./Pages/${name}`),
-    title: title => title ? `${title} - Treblle Task` : 'Treblle Task',
-    setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', {eager: true})
+        let page = pages[`./Pages/${name}.vue`]
+        // page.default.layout = page.default.layout || Layout
+        return page
+    },
+    setup({el, App, props, plugin}) {
+        createApp({render: () => h(App, props)})
             .use(plugin)
             .mount(el)
     },
