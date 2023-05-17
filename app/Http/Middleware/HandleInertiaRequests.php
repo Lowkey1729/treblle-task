0700eca0565008e9012b4ec68913e83e37950510
lowkey1729 <olarewajumojeed9@gmail.com>
2023-05-17 12:41:56 +0100

    Return the details of authenticated users.

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -29,11 +29,24 @@ class HandleInertiaRequests extends Middleware
     /**
      * Defines the props that are shared by default.
      *
+     * @return array<string, array<string, mixed>>
      * @see https://inertiajs.com/shared-data
      */
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
+            'auth' => function () use ($request) {
+                return [
+                    'user' => $request->user() ? [
+                        'uuid' => $request->user()->uuid,
+                        'first_name' => $request->user()->first_name,
+                        'last_name' => $request->user()->last_name,
+                        'email' => $request->user()->email,
+                        'full_name' => $request->user()->full_name,
+                        'phone_number' => $request->user()->phone_number,
+                    ] : null,
+                ];
+            },
             'flash' => function () use ($request) {
                 return [
                     'success' => $request->session()->get('success'),
