78489b34b2fcd4f75355f4a13233c8af9f6cfaf3
lowkey1729 <olarewajumojeed9@gmail.com>
2023-05-19 00:37:07 +0100

    Created a new service provider

--- app/Providers/TreblleServiceProvider.php
+++ app/Providers/TreblleServiceProvider.php
@@ -16,27 +16,40 @@ use App\Services\Contracts\Auth\AuthInterface;
 use App\Services\Contracts\Auth\PasswordInterface;
 use App\Services\Contracts\Auth\RegisterInterFace;
 use App\Services\Contracts\User\UserInterface;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
+class TreblleServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
+     * Register services.
      */
     public function register(): void
     {
+        $this->app->when(APIRegisterUserController::class)
+            ->needs(RegisterInterFace::class)
+            ->give(fn () => new APIRegisterUserAction());
 
+        $this->app->when(APIAuthSessionController::class)
+            ->needs(AuthInterface::class)
+            ->give(fn () => new APIAuthSessionAction());
+
+        $this->app->when(WebRegisterUserController::class)
+            ->needs(RegisterInterFace::class)
+            ->give(fn () => new WebRegisterUserAction());
+
+        $this->app->when(WebAuthSessionController::class)
+            ->needs(AuthInterface::class)
+            ->give(fn () => new WebAuthSessionAction());
+
+        $this->app->bind(UserInterface::class, UserAction::class);
+        $this->app->bind(PasswordInterface::class, PasswordAction::class);
     }
 
     /**
-     * Bootstrap any application services.
+     * Bootstrap services.
      */
     public function boot(): void
     {
-        Model::preventAccessingMissingAttributes();
-        Model::preventSilentlyDiscardingAttributes();
-
-        Model::preventLazyLoading(! app()->isProduction());
+        //
     }
 }
